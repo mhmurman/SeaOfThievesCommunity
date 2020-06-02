6b1a4669b65d018818031c2109b4a2679460cc7b
Mats-HJalmar Murman <matte.hj1234@gmail.com>
2020-06-02 19:52:07 +0200

    Added React (surrender -> Shipmates)

--- include/react.py
+++ include/react.py
@@ -1,4 +1,15 @@
 import discord
 
-def ReactHandler(reaction, user):
-    print(user.displayname + " added a reaction on " + reaction.message)
+async def ReactAddHandler(client, reaction, user):#704076592837820459
+    if reaction.message_id == 717400236758138890 and reaction.emoji.name == "surrender": #message id of the welcome message and id for :aye:
+        server = client.get_guild(reaction.guild_id)
+        member = server.get_member(user.id)
+        role = discord.utils.get(server.roles, name = "Shipmates")
+        await member.add_roles(role)
+
+async def ReactRemoveHandler(client, reaction, user):
+    if reaction.message_id == 717400236758138890 and reaction.emoji.name == "surrender": #message id of the welcome message
+        server = client.get_guild(reaction.guild_id)
+        member = server.get_member(user.id)
+        role = discord.utils.get(server.roles, name = "Shipmates")
+        await member.remove_roles(role)
--- main.py
+++ main.py
@@ -7,9 +7,16 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_react_add(reaction, user):
-    react.ReactHandler(reaction, user)
+async def on_raw_reaction_add(reaction):
+    user = client.get_user(reaction.user_id)
+    await react.ReactAddHandler(client, reaction, user)
+@client.event
+async def on_raw_reaction_remove(reaction):
+    user = client.get_user(reaction.user_id)
+    await react.ReactRemoveHandler(client, reaction, user)
+
 
 f = open("discord_bot_token.txt")
 token = f.read()
+f.close()
 client.run(token)
